c9554625abbd3aed82b95ae92dc087449f9092c1
ABDOCEH <153424684+ABDOCEH@users.noreply.github.com>
2024-01-24 18:05:38 +0100

    Add files via upload

---
+++ portfolio site template/timeTable.js
@@ -0,0 +1,139 @@
+const Sunday =[
+    {   
+        time: '11:00-17:00',
+        roomNumber: 'penetrating networking',
+        subject: 'KALI LINUX',
+        type: 'ETHICAL HACKING'
+    },
+    {   
+        time: '20:00-02:30',
+        roomNumber: 'DATABASE PENETRATING',
+        subject: 'PARROT OS',
+        type: 'ETHICAL HACKING'
+    }
+]
+const Monday =[
+    {  
+        time: '11:00-01:00 PM',
+        roomNumber: 'HTML',
+        subject: 'Tutoriel',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '10-11 PM',
+        roomNumber: 'CSS',
+        subject: 'Tutoriel',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'HTML',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 PM',
+        roomNumber: 'HTML',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '11-12 PM',
+        roomNumber: 'CSS',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'HTML',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'SECURITY ++',
+        subject: 'Tutoriel',
+        type: 'ETHICAL HACKING'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 PM',
+        roomNumber: 'HTML',
+        subject: 'Tutoriel',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '11-12 PM',
+        roomNumber: 'HTML',
+        subject: 'Tutoriel',
+        type: 'Front-End Developer'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 PM',
+        roomNumber: 'CSS',
+        subject: 'Tutoriel',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'HTML',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'HTML',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 PM',
+        roomNumber: 'HTML',
+        subject: 'Tutoriel',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '11-12 PM',
+        roomNumber: 'HTML',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'server penetrating',
+        subject: 'PARROT OS',
+        type: 'ETHICAL HACKING'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 PM',
+        roomNumber: 'CSS',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '10-11 PM',
+        roomNumber: 'HTML',
+        subject: 'VS code',
+        type: 'Front-End Developer'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'data management',
+        subject: 'PARROT OS',
+        type: 'ETHICAL HACKING'
+    }
+]
